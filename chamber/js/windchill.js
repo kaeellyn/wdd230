4ddb49efa88531f4d90232402dbb9e025d83595c
kaeellyn <pap21006@byui.edu>
2022-02-12 02:59:32 +0800

    updated hero img, fixed layout

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,5 +1,5 @@
 const t = parseInt(document.querySelector("#temp").textContent);
-const s = parseInt(document.getElementById("wind-speed").textContent);
+const s = parseInt(document.querySelector("#wind-speed").textContent);
 
 if(t <= 50 && s > 3) {
     const windchill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s,0.16) + 0.4275 * t * Math.pow(s,0.16)
