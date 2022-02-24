56d7d5f296b385887cfff60d4d68c834309cb761
kaeellyn <pap21006@byui.edu>
2022-02-24 23:05:34 +0800

    join page updates

--- chamber/js/dates.js
+++ chamber/js/dates.js
@@ -1,2 +1,5 @@
 const dayJsObject = dayjs();
 document.querySelector('#currentDate').innerHTML = dayJsObject.format('dddd, DD MMMM YYYY')
+
+let dateTime = dayJsObject.format('MMM DD YYYY, dddd | h:m')
+document.querySelector('#submitdate').value = dateTime
